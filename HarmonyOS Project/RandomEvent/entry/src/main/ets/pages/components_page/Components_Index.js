c85090fc1d008a18ee455349c23a81612b3672f3
SoulHarbor <148468166+IsOneHeart@users.noreply.github.com>
2024-10-02 14:47:47 +0800

    Add files via upload

---
+++ HarmonyOS Project/RandomEvent/entry/src/main/ets/pages/components_page/Components_Index.js
@@ -0,0 +1,22 @@
+import router from '@ohos.router';
+struct Components_Index extends   {
+    constructor() { }
+    Component_List() {
+            .width('95%')
+            .height('100%');
+    }
+    //——————————————————————————————————
+    NavigationTitle() {
+        .alignItems(HorizontalAlign.Start);
+        //.width('90%')
+    }
+    NavigationToolbar() {
+        //.margin({ left: 24 })
+            .alignItems(VerticalAlign.Center);
+    }
+    //——————————————————————————————————
+    build() {
+        ;
+    }
+}
+//# sourceMappingURL=Components_Index.js.map
