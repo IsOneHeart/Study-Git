14dae14634f9a9d404de006a524cbdf83b57d9ab
SoulHarbor <148468166+IsOneHeart@users.noreply.github.com>
2024-10-02 14:01:43 +0800

    Delete Java Project/第2章 基本数据类型和数组 directory

--- Java Project/第2章 基本数据类型和数组/Unicode_List/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.printf("你：%d\n", (int) '你');
-        System.out.println("我：" + (int) '我');
-        System.out.print("他：" + (int) '他');
-    }
-}
